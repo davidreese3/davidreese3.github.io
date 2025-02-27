10cfe93818664333011f767dadd6384f28bfa0e4
davidreese3 <david33reese@gmail.com>
2025-02-27 16:22:55 -0500

    Standardized color code strings

--- randomColor.js
+++ randomColor.js
@@ -1,6 +1,6 @@
 function generateGitHubGraph(){
     const graphContainer = document.getElementById('graphContainer');
-    const colorArray = ["ff6f61", "7a2f2f", "e58e26", "c49b0b", "1b4d3e", "15616d", "1c3f60", "512D6D", ""];
+    const colorArray = ["ff6f61", "7a2f2f", "e58e26", "c49b0b", "1b4d3e", "15616d", "1c3f60", "512d6d", ""];
     const randomIndex = Math.floor(Math.random() * colorArray.length);
     let selectedColor = colorArray[randomIndex];
 
