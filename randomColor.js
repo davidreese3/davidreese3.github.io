5927afae0a9c5277168f8b07f6699ab6126a94e9
davidreese3 <david33reese@gmail.com>
2025-02-27 16:06:46 -0500

    Added color styling for the current color code

--- randomColor.js
+++ randomColor.js
@@ -1,6 +1,6 @@
 function generateGitHubGraph(){
     const graphContainer = document.getElementById('graphContainer');
-    const colorArray = ["ff6f61/", "7a2f2f/", "e58e26/", "c49b0b/", "1b4d3e/", "15616d/", "1c3f60/", "512D6D/", ""];
+    const colorArray = ["ff6f61", "7a2f2f", "e58e26", "c49b0b", "1b4d3e", "15616d", "1c3f60", "512D6D", ""];
     const randomIndex = Math.floor(Math.random() * colorArray.length);
     let selectedColor = colorArray[randomIndex];
 
@@ -10,13 +10,15 @@ function generateGitHubGraph(){
     img.className = 'gitHub';
     graphContainer.appendChild(img); 
 
+    const span = document.getElementById('selectedColor');
     if(selectedColor == ""){
-        selectedColor = "GitHub's default contribution color."
+        span.textContent = "GitHub's default contribution color";
     }
     else{
-        selectedColor = "#" + selectedColor.substring(0,6);
+        const hexColor = "#" + selectedColor;
+        span.style.color = hexColor;
+        span.textContent = hexColor;
     }
-    document.getElementById('selectedColor').textContent = selectedColor + ".";
 }
 
 document.addEventListener('DOMContentLoaded', function () {
